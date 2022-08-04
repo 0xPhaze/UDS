8c1970678c272bbb644dcfaacaedf76d92b1db73
0xphaze <0xphaze@gmail.com>
2022-08-04 10:15:33 +0200

    rename erc20ds

--- src/tokens/ERC20RewardUDS.sol
+++ src/tokens/ERC20RewardUDS.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC20UDS, s as erc20DS} from "./ERC20UDS.sol";
+import {ERC20UDS, s as erc20ds} from "./ERC20UDS.sol";
 
 // ------------- storage
 
