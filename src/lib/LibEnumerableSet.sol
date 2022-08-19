64f15b0f9f44a3b50c3809f3cea566e10856ee7b
0xphaze <0xphaze@gmail.com>
2022-08-19 10:27:51 +0200

    add LibEnumerableSet

---
+++ src/lib/LibEnumerableSet.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+struct Bytes32Set {
+    bytes32[] _values;
+    mapping(bytes32 => uint256) _indices;
+}
+
+struct Uint256Set {
+    uint256[] _values;
+    mapping(uint256 => uint256) _indices;
+}
+
+struct AddressSet {
+    address[] _values;
+    mapping(address => uint256) _indices;
+}
+
+/// @title EnumerableSet
+/// @author phaze (https://github.com/0xPhaze/UDS)
+/// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts)
+/// @dev requires adding `using LibEnumerableSet for Uint256Set;`
+library LibEnumerableSet {
+    // ---------------------------------------------------------------------
+    // Bytes32Set
+    // ---------------------------------------------------------------------
+
+    function add(Bytes32Set storage set, bytes32 val) internal returns (bool) {
+        uint256 setIndex = set._indices[val];
+        if (setIndex != 0) return false;
+
+        set._values.push(val);
+        set._indices[val] = set._values.length;
+
+        return true;
+    }
+
+    function remove(Bytes32Set storage set, bytes32 val) internal returns (bool) {
+        uint256 indexToReplace = set._indices[val];
+        if (indexToReplace == 0) return false;
+
+        uint256 lastIndex = set._values.length;
+
+        if (indexToReplace != lastIndex) {
+            bytes32 lastValue = set._values[lastIndex - 1];
+
+            set._values[indexToReplace - 1] = lastValue;
+            set._indices[lastValue] = indexToReplace;
+        }
+
+        set._indices[val] = 0;
+        set._values.pop();
+
+        return true;
+    }
+
+    function includes(Bytes32Set storage set, bytes32 val) internal view returns (bool) {
+        return set._indices[val] != 0;
+    }
+
+    function values(Bytes32Set storage set) internal view returns (bytes32[] memory) {
+        return set._values;
+    }
+
+    function length(Bytes32Set storage set) internal view returns (uint256) {
+        return set._values.length;
+    }
+
+    // ---------------------------------------------------------------------
+    // Uint256Set
+    // ---------------------------------------------------------------------
+
+    function add(Uint256Set storage set, uint256 val) internal returns (bool) {
+        Bytes32Set storage set_;
+        bytes32 val_;
+        assembly {
+            set_.slot := set.slot
+            val_ := val
+        }
+        return add(set_, val_);
+    }
+
+    function remove(Uint256Set storage set, uint256 val) internal returns (bool) {
+        Bytes32Set storage set_;
+        bytes32 val_;
+        assembly {
+            set_.slot := set.slot
+            val_ := val
+        }
+        return remove(set_, val_);
+    }
+
+    function includes(Uint256Set storage set, uint256 val) internal view returns (bool) {
+        return set._indices[val] != 0;
+    }
+
+    function values(Uint256Set storage set) internal view returns (uint256[] memory) {
+        return set._values;
+    }
+
+    function length(Uint256Set storage set) internal view returns (uint256) {
+        return set._values.length;
+    }
+
+    // ---------------------------------------------------------------------
+    // AddressSet
+    // ---------------------------------------------------------------------
+
+    function add(AddressSet storage set, address val) internal returns (bool) {
+        Bytes32Set storage set_;
+        bytes32 val_;
+        assembly {
+            set_.slot := set.slot
+            val_ := val
+        }
+        return add(set_, val_);
+    }
+
+    function remove(AddressSet storage set, address val) internal returns (bool) {
+        Bytes32Set storage set_;
+        bytes32 val_;
+        assembly {
+            set_.slot := set.slot
+            val_ := val
+        }
+        return remove(set_, val_);
+    }
+
+    function includes(AddressSet storage set, address val) internal view returns (bool) {
+        return set._indices[val] != 0;
+    }
+
+    function values(AddressSet storage set) internal view returns (address[] memory) {
+        return set._values;
+    }
+
+    function length(AddressSet storage set) internal view returns (uint256) {
+        return set._values.length;
+    }
+}
---
+++ test/LibEnumerableSet.t.sol
@@ -0,0 +1,342 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import "UDS/lib/LibEnumerableSet.sol";
+
+contract TestEnumerableUint256Set is Test {
+    using LibEnumerableSet for Uint256Set;
+
+    Uint256Set set;
+    uint256[] _setValues;
+
+    /* ------------- O(n) helpers ------------- */
+
+    function indexOfValue(uint256 value) internal view returns (uint256) {
+        for (uint256 i; i < _setValues.length; i++) if (_setValues[i] == value) return i;
+        return type(uint256).max;
+    }
+
+    function valuesInclude(uint256 value) internal view returns (bool) {
+        return indexOfValue(value) != type(uint256).max;
+    }
+
+    function assertEq(Uint256Set storage set_, uint256[] storage values) internal {
+        assertEq(set_.length(), values.length);
+
+        for (uint256 i; i < values.length; i++) {
+            assertTrue(set_.includes(values[i]));
+        }
+    }
+
+    function assertIsUniqueSet(Uint256Set storage set_) internal {
+        uint256[] memory values = set_.values();
+        for (uint256 i; i < values.length; i++) {
+            for (uint256 j = i + 1; j < values.length; j++) {
+                assertTrue(values[i] != values[j]);
+            }
+        }
+    }
+
+    /* ------------- add() ------------- */
+
+    function test_add(uint256 value) public {
+        bool includes = set.includes(value);
+        bool added = set.add(value);
+
+        assertEq(added, !includes);
+        assertTrue(set.includes(value));
+
+        // mirror by performing "dumb" O(n) operations
+
+        assertEq(includes, valuesInclude(value));
+
+        if (!includes) _setValues.push(value);
+
+        assertTrue(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- remove() ------------- */
+
+    function test_rem(uint256 value) public {
+        bool includes = set.includes(value);
+        bool removed = set.remove(value);
+
+        assertEq(removed, includes);
+        assertFalse(set.includes(value));
+
+        // mirror
+
+        assertEq(includes, valuesInclude(value));
+
+        if (includes) {
+            _setValues[indexOfValue(value)] = _setValues[_setValues.length - 1];
+            _setValues.pop();
+        }
+
+        assertFalse(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- add_remove() ------------- */
+
+    function test_add_remove() public {
+        test_rem(3);
+        test_add(1);
+        test_add(2);
+        test_add(3);
+        test_rem(3);
+        test_add(2);
+        test_rem(4);
+        test_add(2);
+        test_rem(3);
+        test_add(2);
+        test_rem(1);
+        test_add(5);
+        test_rem(1);
+        test_add(5);
+        test_add(7);
+        test_rem(8);
+        test_rem(2);
+        test_rem(9);
+        test_rem(3);
+
+        assertEq(set, _setValues);
+    }
+
+    function test_add_remove(uint8[] calldata values) public {
+        for (uint256 i; i < values.length; i++) {
+            uint256 value = values[i];
+            if (value > 100) test_add(value % 100);
+            else test_rem(value % 100);
+        }
+
+        assertEq(set, _setValues);
+    }
+}
+
+contract TestEnumerableBytes32Set is Test {
+    using LibEnumerableSet for Bytes32Set;
+
+    Bytes32Set set;
+    bytes32[] _setValues;
+
+    /* ------------- O(n) helpers ------------- */
+
+    function indexOfValue(bytes32 value) internal view returns (uint256) {
+        for (uint256 i; i < _setValues.length; i++) if (_setValues[i] == value) return i;
+        return type(uint256).max;
+    }
+
+    function valuesInclude(bytes32 value) internal view returns (bool) {
+        return indexOfValue(value) != type(uint256).max;
+    }
+
+    function assertEq(Bytes32Set storage set_, bytes32[] storage values) internal {
+        assertEq(set_.length(), values.length);
+
+        for (uint256 i; i < values.length; i++) {
+            assertTrue(set_.includes(values[i]));
+        }
+    }
+
+    function assertIsUniqueSet(Bytes32Set storage set_) internal {
+        bytes32[] memory values = set_.values();
+        for (uint256 i; i < values.length; i++) {
+            for (uint256 j = i + 1; j < values.length; j++) {
+                assertTrue(values[i] != values[j]);
+            }
+        }
+    }
+
+    /* ------------- add() ------------- */
+
+    function test_add(bytes32 value) public {
+        bool includes = set.includes(value);
+        bool added = set.add(value);
+
+        assertEq(added, !includes);
+        assertTrue(set.includes(value));
+
+        // mirror by performing "dumb" O(n) operations
+
+        assertEq(includes, valuesInclude(value));
+
+        if (!includes) _setValues.push(value);
+
+        assertTrue(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- remove() ------------- */
+
+    function test_rem(bytes32 value) public {
+        bool includes = set.includes(value);
+        bool removed = set.remove(value);
+
+        assertEq(removed, includes);
+        assertFalse(set.includes(value));
+
+        // mirror
+
+        assertEq(includes, valuesInclude(value));
+
+        if (includes) {
+            _setValues[indexOfValue(value)] = _setValues[_setValues.length - 1];
+            _setValues.pop();
+        }
+
+        assertFalse(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- add_remove() ------------- */
+
+    function test_add_remove() public {
+        test_rem(bytes32(uint256(3)));
+        test_add(bytes32(uint256(1)));
+        test_add(bytes32(uint256(2)));
+        test_add(bytes32(uint256(3)));
+        test_rem(bytes32(uint256(3)));
+        test_add(bytes32(uint256(2)));
+        test_rem(bytes32(uint256(4)));
+        test_add(bytes32(uint256(2)));
+        test_rem(bytes32(uint256(3)));
+        test_add(bytes32(uint256(2)));
+        test_rem(bytes32(uint256(1)));
+        test_add(bytes32(uint256(5)));
+        test_rem(bytes32(uint256(1)));
+        test_add(bytes32(uint256(5)));
+        test_add(bytes32(uint256(7)));
+        test_rem(bytes32(uint256(8)));
+        test_rem(bytes32(uint256(2)));
+        test_rem(bytes32(uint256(9)));
+        test_rem(bytes32(uint256(3)));
+
+        assertEq(set, _setValues);
+    }
+
+    function test_add_remove(uint8[] calldata values) public {
+        for (uint256 i; i < values.length; i++) {
+            uint256 value = values[i];
+            if (value > 100) test_add(bytes32(value % 100));
+            else test_rem(bytes32(value % 100));
+        }
+
+        assertEq(set, _setValues);
+    }
+}
+
+contract TestEnumerableAddressSet is Test {
+    using LibEnumerableSet for AddressSet;
+
+    AddressSet set;
+    address[] _setValues;
+
+    /* ------------- O(n) helpers ------------- */
+
+    function indexOfValue(address value) internal view returns (uint256) {
+        for (uint256 i; i < _setValues.length; i++) if (_setValues[i] == value) return i;
+        return type(uint256).max;
+    }
+
+    function valuesInclude(address value) internal view returns (bool) {
+        return indexOfValue(value) != type(uint256).max;
+    }
+
+    function assertEq(AddressSet storage set_, address[] storage values) internal {
+        assertEq(set_.length(), values.length);
+
+        for (uint256 i; i < values.length; i++) {
+            assertTrue(set_.includes(values[i]));
+        }
+    }
+
+    function assertIsUniqueSet(AddressSet storage set_) internal {
+        address[] memory values = set_.values();
+        for (uint256 i; i < values.length; i++) {
+            for (uint256 j = i + 1; j < values.length; j++) {
+                assertTrue(values[i] != values[j]);
+            }
+        }
+    }
+
+    /* ------------- add() ------------- */
+
+    function test_add(address value) public {
+        bool includes = set.includes(value);
+        bool added = set.add(value);
+
+        assertEq(added, !includes);
+        assertTrue(set.includes(value));
+
+        // mirror by performing "dumb" O(n) operations
+
+        assertEq(includes, valuesInclude(value));
+
+        if (!includes) _setValues.push(value);
+
+        assertTrue(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- remove() ------------- */
+
+    function test_rem(address value) public {
+        bool includes = set.includes(value);
+        bool removed = set.remove(value);
+
+        assertEq(removed, includes);
+        assertFalse(set.includes(value));
+
+        // mirror
+
+        assertEq(includes, valuesInclude(value));
+
+        if (includes) {
+            _setValues[indexOfValue(value)] = _setValues[_setValues.length - 1];
+            _setValues.pop();
+        }
+
+        assertFalse(valuesInclude(value));
+        assertIsUniqueSet(set);
+    }
+
+    /* ------------- add_remove() ------------- */
+
+    function test_add_remove() public {
+        test_rem(address(3));
+        test_add(address(1));
+        test_add(address(2));
+        test_add(address(3));
+        test_rem(address(3));
+        test_add(address(2));
+        test_rem(address(4));
+        test_add(address(2));
+        test_rem(address(3));
+        test_add(address(2));
+        test_rem(address(1));
+        test_add(address(5));
+        test_rem(address(1));
+        test_add(address(5));
+        test_add(address(7));
+        test_rem(address(8));
+        test_rem(address(2));
+        test_rem(address(9));
+        test_rem(address(3));
+
+        assertEq(set, _setValues);
+    }
+
+    function test_add_remove(uint8[] calldata values) public {
+        for (uint256 i; i < values.length; i++) {
+            uint256 value = values[i];
+            if (value > 100) test_add(address(uint160(value % 100)));
+            else test_rem(address(uint160(value % 100)));
+        }
+
+        assertEq(set, _setValues);
+    }
+}
